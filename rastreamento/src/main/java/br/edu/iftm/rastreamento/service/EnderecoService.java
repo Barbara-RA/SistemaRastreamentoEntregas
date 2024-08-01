d0a5b40a9d443fb8f98cfca1097a071b7377d3d3
Barbara-RA <barbara.ramosalves94@gmail.com>
2024-08-01 20:58:00 -0300

    CamadaController

---
+++ rastreamento/src/main/java/br/edu/iftm/rastreamento/service/EnderecoService.java
@@ -0,0 +1,12 @@
+package br.edu.iftm.rastreamento.service;
+
+import br.edu.iftm.rastreamento.model.Endereco;
+
+public class EnderecoService {
+
+    public Endereco buscar(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
+
+}
